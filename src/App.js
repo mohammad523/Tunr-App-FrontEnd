15a3b5537a81fcb7aadd901ce756128a420546e8
mxkay <kayalyxvail@gmail.com>
2020-07-16 17:07:31 -0400

    fixed imports

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Playlist from './components/Playlist/Playlist'
 import Favorites from './components/Favorites/Favorites'
-import Header from './components/Header/Header.jsx'
 import NewSong from './components/NewSong/NewSong.jsx'
 
 
