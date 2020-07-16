ba79982adbbd73836e60e51a130301f0b2b6411a
mxkay <kayalyxvail@gmail.com>
2020-07-16 13:24:06 -0400

    Updates

---
+++ precache-manifest.74e2bad0eb165d9ac01a1b4bb1fa81e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82009e8af82e00d405dc6d244afb1709",
+    "url": "/Tunr-App-FrontEnd/index.html"
+  },
+  {
+    "revision": "ec5ad5eb08ea72e8e54a",
+    "url": "/Tunr-App-FrontEnd/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "887e600dc2b829d4f4ce",
+    "url": "/Tunr-App-FrontEnd/static/js/2.e03356e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tunr-App-FrontEnd/static/js/2.e03356e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5ad5eb08ea72e8e54a",
+    "url": "/Tunr-App-FrontEnd/static/js/main.da4e9ec3.chunk.js"
+  },
+  {
+    "revision": "a3508ad4a7083b759fd1",
+    "url": "/Tunr-App-FrontEnd/static/js/runtime-main.f2a78ce4.js"
+  }
+]);
