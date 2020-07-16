df28e01bbaa2359c766c05b4026165b0a47e7124
mxkay <kayalyxvail@gmail.com>
2020-07-16 15:25:55 -0400

    Configured song data with null case

--- src/components/Song/Song.jsx
+++ src/components/Song/Song.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Song.css'
 
-const Song = ({ song }) => {
+const Song = ({ song, toggleFavorite, deleteSong }) => {
+    if(!song) song = { _id: false, title: 'unavailable', artist: 'unavailable', time: 0, isFavorite: false }
+    
+    const seconds = '' + (song.time % 60);
+    const time = Math.floor(song.time / 60) + ':' + (seconds.length===0?'00':seconds.length===1?'0'+seconds:seconds);
+
     return (
         <div className='song-container'>
-            <div className='song-item song-title'>Really Long Song Title to Test the Text Overflow</div>
-            <div className='song-item song-artist'>The Abominable Kay</div>
-            <div className='song-item song-time'>2:45</div>
-            <button className='song-button song-favorite'>❤</button>
-            <button className='song-button song-remove'>X</button>
+            <div className='song-item song-title'>{song.title}</div>
+            <div className='song-item song-artist'>{song.artist}</div>
+            <div className='song-item song-time'>{time}</div>
+            <button className={`song-button song-favorite ${song.isFavorite?'favorite':''}`} onClick={toggleFavorite}>❤</button>
+            <button className='song-button song-remove' onClick={deleteSong}>X</button>
         </div>
     )
 }
