7a4c2f59af648dd8f7f7aedc1df8d941e92053ec
mxkay <kayalyxvail@gmail.com>
2020-07-16 16:03:41 -0400

    Created FavoriteSong (cloned from Song)

---
+++ src/components/FavoriteSong/FavoriteSong.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './FavoriteSong.css'
+
+const FavoriteSong = ({ song, toggleFavorite }) => {
+    if(!song) song = { _id: false, title: 'unavailable', artist: 'unavailable', time: 0, isFavorite: false }
+    
+    const seconds = '' + (song.time % 60);
+    const time = Math.floor(song.time / 60) + ':' + (seconds.length===0?'00':seconds.length===1?'0'+seconds:seconds);
+
+    return (
+        <div className='song-container'>
+            <div className='song-item song-title'>{song.title}</div>
+            <div className='song-item song-artist'>{song.artist}</div>
+            <div className='song-item song-time'>{time}</div>
+            <button className={`song-button song-favorite ${song.isFavorite?'favorite':''}`} onClick={toggleFavorite}>❤</button>
+        </div>
+    )
+}
+
+export default FavoriteSong;
