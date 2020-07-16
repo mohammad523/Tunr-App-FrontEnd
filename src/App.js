a310ab65d72beb1b56891e5d04db3132c0487c03
dychotomy <dychotomy2000@yahoo.com>
2020-07-16 16:32:47 -0400

    toggleFavorite function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header/Header.jsx'
+import Axios from 'axios';
 
 function App() {
+  const [faves, setFaves] = useState(false)
+
+  toggleFavorite = (e) => {
+    e.preventDefault()
+
+    Axios({
+      url: `https://tunr4.herokuapp.com/api/songs/${props.match.params.id}`,
+      method: 'PUT',
+      data: song
+    })
+      .then(() => setFaves(true))
+      .catch(console.error)
+  }
+  // Not sure if you want this functionality.
+  // if (faves) {
+  //   return <Redirect to={`/songs/${props.match.params.id}`} />
+  // }
+
   return (
     <div className="App">
       <Header />
