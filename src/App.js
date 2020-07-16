4edb33c45bc9cb84649ba89520024c20b4d8cefd
Mohammad Hasan <its_mh@outlook.com>
2020-07-16 15:41:41 -0500

    add a working form, the form adds the songs to the database, all work was done in NewSong component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.jsx'
+import NewSong from './components/NewSong/NewSong.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NewSong />
     </div>
   );
 }
